252575f416fa2a028fc9b313a176df0ba839cfe4
Luiz Nascimento <luizh.nnh@gmail.com>
2024-11-27 18:05:54 -0300

    feat(jwt): adicionando auth para proximos passos

---
+++ src/gateways/jwt/jwt.payload.ts
@@ -0,0 +1,12 @@
+import { UserRole } from "@domains/user-role.enum";
+import { Builder, IBuilder } from "builder-pattern";
+
+export class JwtPayload {
+  public readonly id?: string;
+  public readonly role: UserRole;
+  public readonly email: string;
+
+  public static builder(): IBuilder<JwtPayload> {
+    return Builder<JwtPayload>();
+  }
+}
