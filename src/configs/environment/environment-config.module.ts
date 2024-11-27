5de60a52216e4a8157d678947f74b77a14b0c647
Luiz Nascimento <luizh.nnh@gmail.com>
2024-11-27 09:17:54 -0300

    chore(deps): adicionado @nestjs/config

---
+++ src/configs/environment/environment-config.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { EnvironmentConfigService } from "./environment-config.service";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: "./env/.env.development",
+      ignoreEnvFile: process.env.NODE_ENV === "production",
+      isGlobal: true,
+    }),
+  ],
+  providers: [EnvironmentConfigService],
+  exports: [EnvironmentConfigService],
+})
+export class EnvironmentConfigModule {}
---
+++ src/configs/environment/environment-config.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@nestjs/common";
+import type { ConfigService } from "@nestjs/config";
+import type { AppProperties } from "./app.properties";
+import type { AppConfig } from "./app.properties.interface";
+
+@Injectable()
+export class EnvironmentConfigService implements AppConfig {
+  constructor(private configService: ConfigService<AppProperties>) {}
+
+  public getDatabaseHost(): string {
+    return this.configService.get<string>("DATABASE_HOST");
+  }
+
+  public getDatabasePort(): number {
+    return this.configService.get<number>("DATABASE_PORT");
+  }
+
+  public getDatabaseUser(): string {
+    return this.configService.get<string>("DATABASE_USER");
+  }
+
+  public getDatabasePassword(): string {
+    return this.configService.get<string>("DATABASE_PASSWORD");
+  }
+
+  public getDatabaseName(): string {
+    return this.configService.get<string>("DATABASE_NAME");
+  }
+
+  public getDatabaseSchema(): string {
+    return this.configService.get<string>("DATABASE_SCHEMA");
+  }
+
+  public getDatabaseSync(): boolean {
+    return this.configService.get<boolean>("DATABASE_SYNCHRONIZE");
+  }
+}
